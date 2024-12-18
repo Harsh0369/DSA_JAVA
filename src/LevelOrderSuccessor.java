bc3588d0c78a24b0fdd7510901ca6b53aa76c324
Harsh <harshagarwal81528@gmail.com>
2024-12-18 09:02:12 +0530

    java

---
+++ src/LevelOrderSuccessor.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderSuccessor {
+    public TreeNode findSuccessor(TreeNode root,int key)
+    {
+        if(root==null)
+        {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty())
+        {
+            int levelSize = queue.size();
+            TreeNode currentNode = queue.poll();
+            if(currentNode.left!=null)
+            {
+                queue.offer(currentNode.left);
+            }
+            if(currentNode.right!=null)
+            {
+                queue.offer(currentNode.right);
+            }
+            if(currentNode.data == key)
+            {
+                break;
+            }
+        }
+        return queue.peek();
+    }
+}
